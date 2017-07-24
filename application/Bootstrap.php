908607f8e742ab1a9b4444413e583b02e437ec55
unknown <user@.(none)>
2017-07-24 10:59:22 +0200

    ttest

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-
+Zend_View_Helper_PaginationControl::setDefaultViewPartial('controls.phtml');
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
--- application/controllers/BookController.php
+++ application/controllers/BookController.php
@@ -11,13 +11,18 @@ class BookController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
-        $Book=new Application_Model_DbTable_Book();
-		$data = range(1, 100);
+        $db = Zend_Db_Table::getDefaultAdapter(); 
+		$test=4;
+		$test=4;
+		$sql = 'SELECT * FROM book ';
+	    $result = $db->fetchAll($sql);
+	    $page=$this->_getParam('page',1);
+	    $paginator = Zend_Paginator::factory($result);
+	    $paginator->setItemCountPerPage(2);
+	    $paginator->setCurrentPageNumber($page);
+
+        $this->view->paginator=$paginator;
  
-		// Get a Paginator object using Zend_Paginator's built-in factory.
-		$paginator = Zend_Paginator::factory($data);
-		$this->view->paginator=$paginator;
-		
 		
     }
 
